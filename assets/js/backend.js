6454e215ed9b963ff2b9bee1483507b9d6cb1866
tanishk818 <52106030+tanishk818@users.noreply.github.com>
2021-02-25 09:40:24 +0530

    removed a bug

--- assets/js/backend.js
+++ assets/js/backend.js
@@ -3,35 +3,47 @@ clr.sort(() => Math.random() - 0.5);
     var visi=0;
     var match=[];
     var events=[];
+    var locked=true;
     function flip(event){
     //alert(clr[event]+" "+event);
-	var element = document.getElementsByClassName("card");
-	if (element[event].style.transform != "rotateY(180deg)") {
-        element[event].style.transform = "rotateY(180deg)";
-        var element_back = document.getElementsByClassName("back");
-        element_back[event].style.background="url("+clr[event]+")";
-        element_back[event].style.backgroundSize= "cover";
-        match.push(clr[event]);
-        events.push(event);
-        mv++;
-        document.getElementById("text_content").innerHTML = "Moves : "+mv;
-        document.getElementById("score").innerHTML = "Moves : "+mv;
-    }
-    //console.log(match.length);
-    setTimeout(function(){
-            if(match.length==2){
-            if(match[0]===match[1]){
-                visi+=2;
-                if(visi===clr.length) $('#myModal').modal('show');
-                else console.log(match[0]+" "+match[1]+" "+visi);
-            }
-            else{
-                element[events[0]].style.transform="rotateY(0deg)";
-                element[events[1]].style.transform="rotateY(0deg)";
-            }
-            match.splice(0, match.length);
-            events.splice(0, events.length)
+    if(locked){
+        var element = document.getElementsByClassName("card");
+        if (element[event].style.transform != "rotateY(180deg)") {
+            element[event].style.transform = "rotateY(180deg)";
+            var element_back = document.getElementsByClassName("back");
+            element_back[event].style.background="url("+clr[event]+")";
+            element_back[event].style.backgroundSize= "cover";
+            match.push(clr[event]);
+            events.push(event);
+            mv++;
+            document.getElementById("text_content").innerHTML = "Moves : "+mv;
+            document.getElementById("score").innerHTML = "Moves : "+mv;
         }
-    }, 1100);
-    
+        //console.log(match.length);
+        if(match.length==2){
+            locked=false;
+        setTimeout(function(){
+                
+                if(match[0]===match[1]){
+                    visi+=2;
+                    if(visi===clr.length) $('#myModal').modal('show');
+                    else console.log(match[0]+" "+match[1]+" "+visi);
+                    if(element[events[0]].style.transform==="rotateY(180deg)"&& element[events[1]].style.transform==="rotateY(180deg)")
+                        locked=true;
+                }
+                else{
+                    element[events[0]].style.transform="rotateY(0deg)";
+                    element[events[1]].style.transform="rotateY(0deg)";
+                    if(element[events[0]].style.transform==="rotateY(0deg)"&& element[events[1]].style.transform==="rotateY(0deg)")
+                        locked=true;
+                }
+                match.splice(0, match.length);
+                events.splice(0, events.length)
+        }, 700);
+       /* setTimeout(function(){
+            locked=true;
+        },1900);*/
+    }
+        
+    }
 };
